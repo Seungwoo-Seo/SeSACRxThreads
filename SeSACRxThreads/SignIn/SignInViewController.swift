a38b899259751333f6eb927940cb5612b8bbb9d8
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-02 19:26:44 +0900

    [Refactor] SignInViewController Rx+MVVM로리팩토링

--- SeSACRxThreads/SignIn/SignInViewController.swift
+++ SeSACRxThreads/SignIn/SignInViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+
+import RxCocoa
+import RxSwift
 import SnapKit
 
 class SignInViewController: UIViewController {
@@ -14,7 +17,11 @@ class SignInViewController: UIViewController {
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let signInButton = PointButton(title: "로그인")
     let signUpButton = UIButton()
-    
+
+    let disposeBag = DisposeBag()
+
+    let viewModel = SignInViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,10 +31,52 @@ class SignInViewController: UIViewController {
         configure()
         
         signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+
+        bind()
     }
     
     @objc func signUpButtonClicked() {
         navigationController?.pushViewController(SignUpViewController(), animated: true)
+    }
+
+    func bind() {
+        // MARK: - Input
+        emailTextField.rx.text.orEmpty
+            .bind(with: self) { // -> subscribe나 bind말고 좀 더 적절한게 있을꺼 같은데
+                $0.viewModel.email.onNext($1)
+            }
+            .disposed(by: disposeBag)
+
+        passwordTextField.rx.text.orEmpty
+            .bind(with: self) {
+                $0.viewModel.password.onNext($1)
+            }
+            .disposed(by: disposeBag)
+
+        signInButton.rx.tap
+            // subscribe를 사용할 건지 bind를 사용할건지는 이 중간 스트림에 따라서 바뀌겠네
+            .subscribe(with: self) { owner, value in
+                print("Selected")
+            }
+//            .bind(with: self) { owner, value in
+//                print("Selected")
+//            }
+            .disposed(by: disposeBag)
+
+        // MARK: - Output
+        viewModel.validation
+            .bind(to: signInButton.rx.isEnabled)
+            .disposed(by: disposeBag)   // 굳이 메모리 관리를 해줘야 하는 이유가 원래 사라져야하는 인스턴스가 살아 있기 때문인가?
+
+        // validation애서 완료나 에러를 방출할 수 있지만
+        viewModel.validation
+            .bind(with: self) { owner, value in
+                // UI와 관련된 작업을 할 것이기 때문에 절대 완료나 에러를 방출하게 두면 안된다.
+                owner.signInButton.backgroundColor = value ? UIColor.blue : UIColor.red
+                owner.emailTextField.layer.borderColor = value ? UIColor.blue.cgColor : UIColor.red.cgColor
+                owner.passwordTextField.layer.borderColor = value ? UIColor.blue.cgColor : UIColor.red.cgColor
+            }
+            .disposed(by: disposeBag)
     }
     
     
--- SeSACRxThreads/SignIn/SignInViewModel.swift
+++ SeSACRxThreads/SignIn/SignInViewModel.swift
@@ -6,9 +6,27 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 class SignInViewModel {
-    
-    
+    let disposeBag = DisposeBag()
+
+    let email = BehaviorSubject(value: "")
+    let password = BehaviorSubject(value: "")
+
+    let validation: Observable<Bool>
+
+    init() {
+        // 아 착각하고 있던게 Observable 스트림을 시작할 땐 무조건 create, just, of, from으로 시작해야 하는 줄 알았는데
+        // 생각해보니까 결국 걔네도 다 operater네 결국 여러 operater들을 거쳐서 데이터가 가공되고 가공된 데이터를 옵저버가 마지막에
+        // 처리해주는 Flow로 가면 이지하네
+        self.validation = Observable
+            .combineLatest(email, password) {
+                $0.count > 8 && $1.count >= 6
+            }
+        // 원래 이 시점에 validation에서 참조하고 있는 인스턴스는 더 이상 사용되지 않기 때문에 소멸되야하는거 아니냐?
+        // 해답 : Observable은 보통의 클래스 인스턴스의 생명주기와 약간 다르게 동작한다고 한다.
+    }
     
 }
